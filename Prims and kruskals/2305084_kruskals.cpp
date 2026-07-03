794a80353792a8016fc9f459b09845119859e5f4
Najmus Sakib <sakibthoha5@gmail.com>
2026-07-03 21:16:46 +0600

    updated prims and kruskals

---
+++ Prims and kruskals/2305084_kruskals.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Disjoint_Set_Union
+{
+private:
+    vector<long long> parent;
+    vector<long long> rank;
+
+public:
+    Disjoint_Set_Union(long long n)
+    {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (long long i = 0; i < n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+    long long find(long long x)
+    {
+        if (parent[x] != x)
+        {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    bool unite_node(long long x, long long y)
+    {
+        long long rootX = find(x);
+        long long rootY = find(y);
+        if (rootX == rootY)
+        {
+            return false;
+        }
+
+        if (rank[rootX] < rank[rootY])
+        {
+            parent[rootX] = rootY;
+        }
+        else if (rank[rootX] > rank[rootY])
+        {
+            parent[rootY] = rootX;
+        }
+        else
+        {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        return true;
+    }
+};
+bool compare_edges(const pair<long long, pair<long long, long long>> &a, const pair<long long, pair<long long, long long>> &b)
+{
+    if (a.first != b.first)
+        return a.first < b.first;
+    if (a.second.first != b.second.first)
+        return a.second.first < b.second.first;
+
+    return a.second.second < b.second.second;
+}
+void kruskal_algorithm(vector<pair<long long, pair<long long, long long>>> &edges, long long n)
+{
+    sort(edges.begin(), edges.end(), compare_edges);
+
+    Disjoint_Set_Union dsu(n);
+    vector<pair<long long, long long>> MSTedges;
+    long long totalWeight = 0;
+
+    for (vector<pair<long long, pair<long long, long long>>>::iterator it = edges.begin(); it != edges.end(); it++)
+    {
+        long long weight = it->first;
+        long long u = it->second.first;
+        long long v = it->second.second;
+
+        if (dsu.unite_node(u, v))
+        {
+            MSTedges.push_back(make_pair(u, v));
+            totalWeight += weight;
+        }
+    }
+    cout << "Total weight " << totalWeight << endl;
+    for (vector<pair<long long, long long>>::iterator it = MSTedges.begin(); it != MSTedges.end(); ++it)
+    {
+        pair<long long, long long> mst_edge = *it;
+        cout << mst_edge.first << " " << mst_edge.second << endl;
+    }
+}
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+
+    vector<vector<pair<long long, long long>>> adj(n);
+    vector<pair<long long, pair<long long, long long>>> edges;
+
+    for (long long i = 0; i < m; i++)
+    {
+        long long u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+
+        edges.push_back(make_pair(w, make_pair(u, v)));
+    }
+
+    long long root;
+    cin >> root;
+
+    kruskal_algorithm(edges, n);
+}
---
+++ Prims and kruskals/2305084_prims.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+void prim_algorithm(long long root, vector<vector<pair<long long, long long> > > &adj, long long n) {
+    vector<bool> visited(n, false);
+    priority_queue<pair<long long, long long>, vector<pair<long long, long long> >, greater<pair<long long, long long> > > pq;
+    vector<pair<long long, long long> > MSTedges;
+    vector<long long> parent(n, -1);
+    vector<long long> minEdge(n, LLONG_MAX);
+
+    long long totalWeight = 0;
+    pq.push(make_pair(0, root));
+    minEdge[root] = 0;
+
+    while (!pq.empty()) {
+        pair<long long, long long> edge = pq.top();
+        pq.pop();
+
+        long long current_node = edge.second;
+        long long current_weight = edge.first;
+
+        if (visited[current_node])
+            continue;
+
+        if (current_weight != minEdge[current_node])
+            continue;
+
+        visited[current_node] = true;
+        totalWeight += current_weight;
+
+        if (parent[current_node] != -1) {
+            MSTedges.push_back(make_pair(parent[current_node], current_node));
+        }
+
+        for (vector<pair<long long, long long> >::iterator it = adj[current_node].begin(); it != adj[current_node].end(); ++it) {
+            pair<long long, long long> neighbor = *it;
+            long long neighbor_node = neighbor.first;
+            long long neighbor_weight = neighbor.second;
+
+            if (!visited[neighbor_node] && neighbor_weight < minEdge[neighbor_node]) {
+                minEdge[neighbor_node] = neighbor_weight;
+                parent[neighbor_node] = current_node;
+                pq.push(make_pair(neighbor_weight, neighbor_node));
+            }
+        }
+    }
+
+    cout << "Total weight " << totalWeight << endl;
+    cout << "Root node " << root << endl;
+    for (vector<pair<long long, long long> >::iterator it = MSTedges.begin(); it != MSTedges.end(); ++it) {
+        pair<long long, long long> mst_edge = *it;
+        cout << mst_edge.first << " " << mst_edge.second << endl;
+    }
+}
+int main() {
+    long long n, m;
+    cin >> n >> m;
+    
+    vector<vector<pair<long long, long long> > > adj(n);
+    vector<pair<long long, pair<long long, long long> > > edges;
+
+    for (long long i = 0; i < m; i++) {
+        long long u, v, w;
+        cin >> u >> v >> w;
+        
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+        
+        edges.push_back(make_pair(w, make_pair(u, v)));
+    }
+
+    long long root;
+    cin >> root;
+
+    prim_algorithm(root, adj, n);
+    
+    
+}
