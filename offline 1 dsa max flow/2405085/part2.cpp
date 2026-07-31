32e0977513124b296ad9653680ff873e339c6bf6
Najmus Sakib <sakibthoha5@gmail.com>
2026-07-31 22:55:22 +0600

    update

--- offline 1 dsa max flow/2405085/part2.cpp
+++ offline 1 dsa max flow/2405085/part2.cpp
@@ -5,27 +5,27 @@
 #include <map>
 using namespace std;
 
-struct Edge {
+struct edge {
     int from, to, cap;
 };
 
 int n;
-vector<Edge> edges;
+vector<edge> edges;
 vector<vector<int>> adj;
 
-int addEdge(int u, int v, int w) {
-    int edgeId = edges.size();
-    adj[u].push_back(edgeId);
+int addedge(int u, int v, int w) {
+    int edgeid = edges.size();
+    adj[u].push_back(edgeid);
     edges.push_back({u, v, w});
-    adj[v].push_back(edgeId + 1);
+    adj[v].push_back(edgeid + 1);
     edges.push_back({v, u, 0});
-    return edgeId;
+    return edgeid;
 }
 
-long long maxFlowEdmondsKarp(int s, int t) {
-    long long maxFlow = 0;
+long long maxflow(int s, int t) {
+    long long maxflow = 0;
     while (true) {
-        vector<int> parentEdge(n, -1);
+        vector<int> parentedge(n, -1);
         vector<bool> visited(n, false);
         queue<int> q;
         q.push(s);
@@ -33,11 +33,11 @@ long long maxFlowEdmondsKarp(int s, int t) {
 
         while (!q.empty() && !visited[t]) {
             int u = q.front(); q.pop();
-            for (int edgeId : adj[u]) {
-                int v = edges[edgeId].to;
-                if (!visited[v] && edges[edgeId].cap > 0) {
+            for (int edgeid : adj[u]) {
+                int v = edges[edgeid].to;
+                if (!visited[v] && edges[edgeid].cap > 0) {
                     visited[v] = true;
-                    parentEdge[v] = edgeId;
+                    parentedge[v] = edgeid;
                     q.push(v);
                 }
             }
@@ -45,33 +45,33 @@ long long maxFlowEdmondsKarp(int s, int t) {
 
         if (!visited[t]) break;
 
-        int pathFlow = INT_MAX;
+        int pathflow = INT_MAX;
         for (int v = t; v != s; ) {
-            int edgeId = parentEdge[v];
-            pathFlow = min(pathFlow, edges[edgeId].cap);
-            v = edges[edgeId].from;
+            int edgeid = parentedge[v];
+            pathflow = min(pathflow, edges[edgeid].cap);
+            v = edges[edgeid].from;
         }
 
         for (int v = t; v != s; ) {
-            int edgeId = parentEdge[v];
-            edges[edgeId].cap -= pathFlow;
-            edges[edgeId ^ 1].cap += pathFlow;
-            v = edges[edgeId].from;
+            int edgeid = parentedge[v];
+            edges[edgeid].cap -= pathflow;
+            edges[edgeid ^ 1].cap += pathflow;
+            v = edges[edgeid].from;
         }
 
-        maxFlow += pathFlow;
+        maxflow += pathflow;
     }
-    return maxFlow;
+    return maxflow;
 }
 
 int main() {
     int F;
     cin >> F;
 
-    vector<string> id(F), depCity(F), arrCity(F);
-    vector<int> depTime(F), arrTime(F);
+    vector<string> id(F), departurecity(F), arrivalcity(F);
+    vector<int> deptime(F), arrtime(F);
 
-    auto toMinutes = [](string t) {
+    auto convertminute = [](string t) {
         int hh = stoi(t.substr(0, 2));
         int mm = stoi(t.substr(3, 2));
         return hh * 60 + mm;
@@ -79,9 +79,9 @@ int main() {
 
     for (int i = 0; i < F; i++) {
         string dt, at;
-        cin >> id[i] >> depCity[i] >> arrCity[i] >> dt >> at;
-        depTime[i] = toMinutes(dt);
-        arrTime[i] = toMinutes(at);
+        cin >> id[i] >> departurecity[i] >> arrivalcity[i] >> dt >> at;
+        deptime[i] = convertminute(dt);
+        arrtime[i] = convertminute(at);
     }
 
     int source = 0;
@@ -89,14 +89,14 @@ int main() {
     n = 2 * F + 2;
     adj.assign(n, {});
 
-    auto outNode = [&](int i) { return i + 1; };
-    auto inNode  = [&](int i) { return F + i + 1; };
+    auto outnode = [&](int i) { return i + 1; };
+    auto innode  = [&](int i) { return F + i + 1; };
 
     const int TURNAROUND = 180;
 
     for (int i = 0; i < F; i++) {
-        addEdge(source, outNode(i), 1);
-        addEdge(inNode(i), sink, 1);
+        addedge(source, outnode(i), 1);
+        addedge(innode(i), sink, 1);
     }
 
     map<pair<int,int>, int> connectionEdge;
@@ -104,38 +104,38 @@ int main() {
     for (int i = 0; i < F; i++) {
         for (int j = 0; j < F; j++) {
             if (i == j) continue;
-            if (arrCity[i] == depCity[j] && arrTime[i] + TURNAROUND <= depTime[j]) {
-                int edgeId = addEdge(outNode(i), inNode(j), 1);
-                connectionEdge[{i, j}] = edgeId;
+            if (arrivalcity[i] == departurecity[j] && arrtime[i] + TURNAROUND <= deptime[j]) {
+                int edgeid = addedge(outnode(i), innode(j), 1);
+                connectionEdge[{i, j}] = edgeid;
             }
         }
     }
 
-    long long matching = maxFlowEdmondsKarp(source, sink);
-    int numAircraft = F - (int)matching;
+    long long matching = maxflow(source, sink);
+    int aircraftnumber = F - (int)matching;
 
 
-    vector<int> nextFlight(F, -1);
-    vector<bool> hasIncoming(F, false);
+    vector<int> nextflight(F, -1);
+    vector<bool> incoming(F, false);
 
-    for (auto &[pair_ij, edgeId] : connectionEdge) {
+    for (auto &[pair_ij, edgeid] : connectionEdge) {
         int i = pair_ij.first;
         int j = pair_ij.second;
-        if (edges[edgeId].cap == 0) {
-            nextFlight[i] = j;
-            hasIncoming[j] = true;
+        if (edges[edgeid].cap == 0) {
+            nextflight[i] = j;
+            incoming[j] = true;
         }
     }
 
-    cout << "Number of Aircraft: " << numAircraft << "\n";
-    int aircraftCount = 0;
+    cout << "Number of Aircraft: " << aircraftnumber << "\n";
+    int aircraftcount = 0;
     for (int i = 0; i < F; i++) {
-        if (!hasIncoming[i]) {
-            aircraftCount++;
-            cout << "Aircraft " << aircraftCount << ": " << id[i];
+        if (!incoming[i]) {
+            aircraftcount++;
+            cout << "Aircraft " << aircraftcount << ": " << id[i];
             int cur = i;
-            while (nextFlight[cur] != -1) {
-                cur = nextFlight[cur];
+            while (nextflight[cur] != -1) {
+                cur = nextflight[cur];
                 cout << " -> " << id[cur];
             }
             cout << "\n";
